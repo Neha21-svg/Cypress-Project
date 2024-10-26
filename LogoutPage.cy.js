cca8b6dcd0aded71084559368e5e9981e720e885
Neha21-svg <nehakumari240602@gmail.com>
2024-10-26 08:57:44 +0530

    Add files via upload

---
+++ LogoutPage.cy.js
@@ -0,0 +1,13 @@
+class LogoutPage1 {
+    
+    //Logging out from my account
+    Logout(){
+        cy.get('.caret').click();
+    
+        cy.xpath('(//a[@href="https://tutorialsninja.com/demo/index.php?route=account/logout"])[2]').click();
+    }
+    Clicking_on_Continue(){
+        cy.xpath('(//a[@href="https://tutorialsninja.com/demo/index.php?route=common/home"])[3]').click();
+    }
+}
+export default LogoutPage1;
